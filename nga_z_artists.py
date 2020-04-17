4f8497e9bd15a24023416d0fe1a28d21ed3e9aa8
Cecile-Hirschauer <cecile.h.75@gmail.com>
2020-04-17 18:53:59 +0200

    Retrieving Related Pages

--- nga_z_artists.py
+++ nga_z_artists.py
@@ -1,41 +1,28 @@
+
 import requests
 import csv
 from bs4 import BeautifulSoup
 
+f = csv.writer(open('z-artist-names.csv', "w"))
+f.writerow(['Name', 'Links'])
 
-#  1 - Collect and parse first page
-page = requests.get('https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm')
-soup = BeautifulSoup(page.text, 'html.parser')
-
-# 4 - Remove bottom links
-last_links = soup.find(class_='AlphaNav')
-last_links.decompose()
-
-# 6 - Create a file to write to, add headers row
-f = csv.writer(open('z-artist-names.csv', 'w'))
-f.writerow(['Name', 'Link'])
-
-# 2 - Pull all text from the BodyText div
-artist_name_list = soup.find(class_='BodyText')
-
-# 2 - Pull text from all instances of <a> tag within BodyText div
-artist_name_list_items = artist_name_list.find_all('a')
-
-# 3 - create a loop to print out all artists' names
-#for artist_name in artist_name_list_items:
-#   print(artist_name.prettify())
-
-# 5 - use .content to pull out the <a> tag's children
-for artist_name in artist_name_list_items:
-    names = artist_name.contents[0]
-    links = 'https://web.archive.org' + artist_name.get('href')
-    print(names)
-    print(links)
+pages = []
+for i in range(1, 5):
+    url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ' + str(i) + '.htm'
+    pages.append(url)
     
-    # 6 - Add each artist's name and associated link to a raw
-    f.writerow([names, links])    
-
-
-
-
-
+for item in pages:
+    page = requests.get(item)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    
+    list_link = soup.find(class_='AlphaNav')
+    list_link.decompose()
+    
+    artist_name_list = soup.find(class_='BodyText')
+    artist_name_list_items = artist_name_list.find_all('a')
+    
+    for artist_name in artist_name_list_items:
+        names = artist_name.contents[0]
+        links = 'https://web.archive.org' + artist_name.get('href')
+        
+        f.writerow([names, links])
